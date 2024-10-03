b78dab43be70aa54b348536f9589d8cdd57f6a1c
Nykaj A K <2nykajak@gmail.com>
2024-10-03 21:40:32 +0530

    Clean copy

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,13 @@
 from api import app,db
-from flask import jsonify
+from flask import jsonify,request
+import json
 
 @app.route("/",methods=["GET"])
-def home():
+def test():
     obj = db.users.find_one({"name":"Dummy"})
-
     return jsonify({"name":obj["name"]})
+
+# @app.route("/users",methods=["POST"])
+# def add_user():
+#     print(request.form)
+#     return jsonify(request.form.to_dict())
