08aead786b75f324e974767e74bb8cdc5c09ad44
Nykaj A K <2nykajak@gmail.com>
2024-10-03 22:28:08 +0530

    Added create and view routes for users. Note: currently unvalidated

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,6 @@
 from api import app,db
-from flask import jsonify,request
+from flask import jsonify,request,redirect
+from flask_pymongo import ObjectId
 import json
 
 @app.route("/",methods=["GET"])
@@ -7,7 +8,22 @@ def test():
     obj = db.users.find_one({"name":"Dummy"})
     return jsonify({"name":obj["name"]})
 
-# @app.route("/users",methods=["POST"])
-# def add_user():
-#     print(request.form)
-#     return jsonify(request.form.to_dict())
+@app.route("/user/<id>",methods=["GET"])
+def get_user(id):
+    obj = db.users.find_one({"_id":ObjectId(id)})
+    obj = {x:str(y) for x,y in obj.items()}
+    return jsonify({"payload":obj})
+
+@app.route("/users",methods=["POST"])
+def add_user():
+    email = request.form.get("email")
+    name = request.form.get("name")
+    password = request.form.get("password")
+
+    id = db.users.insert_one({
+        "name":name,
+        "email":email,
+        "password":password
+    }).inserted_id
+
+    return redirect(f"/user/{id}")
