b4aa9434de57641c74c67d81567b9c3f7398eb55
Nykaj A K <2nykajak@gmail.com>
2024-10-04 20:01:20 +0530

    Added basic JWT auth route /login to get JWT token and /test to show currently logged in user

--- api/__init__.py
+++ api/__init__.py
@@ -2,13 +2,16 @@ import os
 from flask import Flask,current_app
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["MONGO_URI"] = os.getenv("CONN_STR")
+app.config["SECRET_KEY"] = "verysecretkey" # To be changed
 
 with app.app_context():
     db = PyMongo(current_app).cx["innovise"]
+    jwt = JWTManager(current_app)
 
 CORS(app)
 
--- api/routes.py
+++ api/routes.py
@@ -1,18 +1,36 @@
-from api import app,db
+from api import app,db,jwt
 from flask import jsonify,request,redirect
 from flask_pymongo import ObjectId
-import json
+from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required
 
 @app.route("/",methods=["GET"])
 def test():
     obj = db.users.find_one({"name":"Dummy"})
     return jsonify({"name":obj["name"]})
 
+@app.route("/test",methods=["GET"])
+@jwt_required()
+def test2():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username", None)
+    password = request.form.get("password", None)
+
+    user = db.users.find_one({"name":username})
+    if user and user["password"] == password:
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token)
+    
+    return jsonify({"msg": "Bad username or password"}), 401
+
 @app.route("/users",methods=["GET"])
 def all_users():
     users = db.users.find()
     return jsonify({"payload":[str(u["_id"]) for u in users]})
-
 
 @app.route("/user/<id>",methods=["GET"])
 def get_user(id):
