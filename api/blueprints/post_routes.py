5f1d56d967df4eebdc5af13b8b07223219aab9a2
Nykaj A K <2nykajak@gmail.com>
2024-10-18 14:44:32 +0530

    Added route for recommending post suggestions

--- api/blueprints/post_routes.py
+++ api/blueprints/post_routes.py
@@ -5,6 +5,73 @@ from pymongo.errors import DuplicateKeyError
 from flask_jwt_extended import get_jwt_identity,jwt_required
 from api.blueprints.user_routes import user_routes 
 
+@user_routes.get("/post/suggestions")
+@jwt_required()
+def suggest_posts():
+    """
+        GET /users/post/suggestions
+        Returns a list of post suggestions for logged in user. Can't be their own posts.
+    """
+    current_user = get_jwt_identity()
+    user = db.users.find_one({"name":current_user})
+    
+    own_posts = db.posts.find({"user_id":str(user["_id"])})
+    own_posts = [str(x["_id"]) for x in own_posts]
+    interests = db.interests.find({"user_id":str(user["_id"])})
+    l = [x["tag_id"] for x in interests]
+    
+    res = db.topics.aggregate([
+        {
+            "$match" : {
+                "post_id": {
+                    "$nin": own_posts
+                },
+                "tag_id" : {
+                    "$in" : l
+                },
+            }
+        },
+        {
+            "$group": {
+                "_id" : "$post_id",
+                "total" : {"$sum" : 1} 
+            }
+        },
+        {
+            "$sort" : {
+                "total" : -1
+            }
+        },
+        {
+            "$limit":4
+        }
+    ])
+
+
+    l = [ObjectId(x["_id"]) for x in res]
+    posts = db.posts.find({"_id":{"$in":l}})
+    temp = {str(x["_id"]):{a:str(b) for a,b in x.items()} for x in posts}
+    posts = [temp[str(i)] for i in l]
+    found_posts = []
+    for p in posts:    
+        t_ids = [ObjectId(x["tag_id"]) for x in db.topics.find({"post_id":str(p["_id"])})]
+
+        post_tags = db.tags.aggregate([
+            {
+                "$match": {
+                    "_id": {
+                        "$in" : t_ids
+                    }
+                }
+            }
+        ])
+        post_tags = [x["name"].title() for x in post_tags]
+        d = {x:str(y) for x,y in p.items()}
+        d["tags"] = post_tags
+        found_posts.append(d)
+
+    return jsonify(payload=found_posts)
+
 @user_routes.get("/post/<uid>")
 def see_posts(uid):
     """
