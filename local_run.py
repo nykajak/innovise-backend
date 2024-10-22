cd3faf4ab088777c315902d9835ef98bb6b4b0d9
Swayam <98643120+trex2004@users.noreply.github.com>
2024-10-22 15:16:36 +0530

    changed local_run

--- local_run.py
+++ local_run.py
@@ -1,4 +1,4 @@
 import os
-os.environ['CONN_STR'] = "mongodb+srv://swayam:flWsJRIW2tTIubUq@innovise.fvnxi.mongodb.net/?retryWrites=true&w=majority&appName=Innovise" # Enter your connection URI
+os.environ['CONN_STR'] = "" # Enter your connection URI
 
 import run
